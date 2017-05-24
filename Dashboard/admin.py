31387d0fdc345035146ad29ec2258d4c866ee605
Christian Federmann <cfedermann@gmail.com>
2017-05-23 22:26:34 -0700

    Added UserInviteToken and TimedKeyValueData models.

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserInviteToken, TimedKeyValueData
+
+admin.site.register(UserInviteToken)
+admin.site.register(TimedKeyValueData)
