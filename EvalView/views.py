6eb6132f12ab67452741ff6a542414d04ee07539
Christian Federmann <cfedermann@gmail.com>
2017-06-01 08:36:32 -0700

    Added timing support for direct assessment

--- EvalView/views.py
+++ EvalView/views.py
@@ -12,6 +12,17 @@ LOGGER = logging.getLogger('Dashboard.views')
 LOGGER.addHandler(LOG_HANDLER)
 
 
+from datetime import timedelta
+def seconds_to_timedelta(value):
+    """
+    Converst the given value in secodns to datetime.timedelta.
+    """
+    _days =  value // 86400
+    _hours = (value // 3600) % 24
+    _mins = (value // 60) % 60
+    _secs = value % 60
+    return timedelta(days=_days, hours=_hours, minutes=_mins, seconds=_secs)
+
 # pylint: disable=C0330
 @login_required
 def direct_assessment(request):
@@ -24,7 +35,14 @@ def direct_assessment(request):
     if request.method == "POST":
         score = request.POST.get('score', None)
         item_id = request.POST.get('item_id', None)
+        start_timestamp = request.POST.get('start_timestamp', None)
+        end_timestamp = request.POST.get('end_timestamp', None)
         LOGGER.info('score={0}, item_id={1}'.format(score, item_id))
+        if start_timestamp and end_timestamp:
+            duration = float(end_timestamp) - float(start_timestamp)
+            LOGGER.debug(float(start_timestamp))
+            LOGGER.debug(float(end_timestamp))
+            LOGGER.info('start={0}, end={1}, duration={2}'.format(start_timestamp, end_timestamp, duration))
 
         # If item_id is valid, create annotation result
 
