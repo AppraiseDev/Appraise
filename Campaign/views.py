6ac4e29e0bc12b3ac2d5c1500bd68ce0f877fbfc
Christian Federmann <cfedermann@gmail.com>
2018-01-12 15:50:02 -0800

    Added first_modified and delta to campaign status view

--- Campaign/views.py
+++ Campaign/views.py
@@ -23,7 +23,19 @@ def campaign_status(request, campaign_name, sort_key=2):
             ).values_list('end_time', flat=True)
 
             _annotations = len(_data)
+            _first_modified = seconds_to_timedelta(min(_data)) if len(_data) else None
             _last_modified = seconds_to_timedelta(max(_data)) if len(_data) else None
+            _delta = 'n/a'
+            if _first_modified and _last_modified:
+                _delta = str(_last_modified - _first_modified).split('.')[0]
+
+            if _first_modified:
+                _date_modified = datetime(1970, 1, 1) + _first_modified
+                _first_modified = str(_date_modified).split('.')[0]
+
+            else:
+                _first_modified = 'Never'
+
             if _last_modified:
                 _date_modified = datetime(1970, 1, 1) + _last_modified
                 _last_modified = str(_date_modified).split('.')[0]
@@ -31,13 +43,14 @@ def campaign_status(request, campaign_name, sort_key=2):
             else:
                 _last_modified = 'Never'
 
-            _item = (user.username, _annotations, _last_modified)
+            _item = (user.username, _annotations, _first_modified, _last_modified, _delta)
             _out.append(_item)
 
     _out.sort(key=lambda x: x[int(sort_key)])
-    _txt = []
+    _header = '\t'.join(('username', 'annotations', 'first_modified', 'last_modified', 'delta'))
+    _txt = [_header]
     for x in _out:
-        _local_out = '{0:>20}\t{1:3}\t{2}'.format(*x)
+        _local_out = '{0:>20}\t{1:3}\t{2}\t{3}\t{4}'.format(*x)
         _txt.append(_local_out)
 
     return HttpResponse(u'\n'.join(_txt), content_type='text/plain')
