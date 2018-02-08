1644b2c6171cef0f8eaf631bf890bcbb53f1e519
Christian Federmann <cfedermann@gmail.com>
2018-02-07 21:18:00 -0800

    Added standardised computation of CHK reliability and --export-csv support.

--- Campaign/management/commands/ComputeAnnotatorMetrics.py
+++ Campaign/management/commands/ComputeAnnotatorMetrics.py
@@ -23,6 +23,14 @@ class Command(BaseCommand):
           '--exclude-ids', type=str,
           help='User IDs which should be ignored'
         )
+        parser.add_argument(
+          '--export-csv', action='store_true',
+          help='Exports CSV data in machine readable format'
+        )
+        parser.add_argument(
+          '--chk-threshold', type=float, default=0.5,
+          help='Absolute z threshold below which CHK items are equal'
+        )
         # TODO: add argument to specify batch user
 
     def handle(self, *args, **options):
@@ -30,11 +38,13 @@ class Command(BaseCommand):
         csv_file = options['csv_file']
         exclude_ids = [x.lower() for x in options['exclude_ids'].split(',')] \
           if options['exclude_ids'] else []
+        export_csv = options['export_csv']
+        chk_threshold = options['chk_threshold']
 
-        normalized_scores = OrderedDict()
         if csv_file:
-            _msg = 'Processing annotations in file {0}\n\n'.format(csv_file)
-            self.stdout.write(_msg)
+            if not export_csv:
+                _msg = 'Processing annotations in file {0}\n\n'.format(csv_file)
+                self.stdout.write(_msg)
 
             # Need to load data from CSV file and bring into same
             # format as would have been produced by the call to
@@ -68,8 +78,9 @@ class Command(BaseCommand):
             # Identify Campaign instance for given name
             campaign = Campaign.objects.filter(campaignName=campaign_name).first()
             if not campaign:
-                _msg = 'Failure to identify campaign {0}'.format(campaign_name)
-                self.stdout.write(_msg)
+                if not export_csv:
+                    _msg = 'Failure to identify campaign {0}'.format(campaign_name)
+                    self.stdout.write(_msg)
                 return
 
             csv_data = DirectAssessmentResult.get_system_data(campaign.id, extended_csv=True)
@@ -90,6 +101,23 @@ class Command(BaseCommand):
                 )
 
                 user_scores[_key].append((_segment_id, _system_id, _type,  _score))
+
+        user_means = defaultdict(float)
+        user_stdev = defaultdict(float)
+
+        from math import sqrt
+        for key, value in user_scores.items():
+            _scores = [x[3] for x in value]
+            user_means[key] = sum(_scores) / len(_scores) if len(_scores) else 0
+            user_stdev[key] = sqrt( sum( ( (x - user_means[key]) ** 2 / (len(_scores) - 1) ) for x in _scores ) )
+
+        user_z_scores = defaultdict(list)
+        for key, values in user_scores.items():
+            for value in values:
+                z_score = (value[3] - user_means[key]) / user_stdev[key]
+                user_z_scores[key].append((value[0], value[1], value[2], z_score))
+
+        user_scores = user_z_scores
 
         metrics = defaultdict(list)
         for key, value in user_scores.items():
@@ -118,7 +146,7 @@ class Command(BaseCommand):
                     continue
 
                 _key = '{0}-{1}'.format(x[0], x[1])
-                _fourScore = min(int(x[3]/25.) + 1, 4)
+                _fourScore = x[3] # min(int(x[3]/25.) + 1, 4)
                 _scores[_key].append((_fourScore, x[2]))
 
             _potential = 0
@@ -128,7 +156,8 @@ class Command(BaseCommand):
                     _potential += 1
                     _data = item[1]
                     _data.sort(key=lambda x: x[1])
-                    if _data[0][0] == _data[1][0]:
+                    # if _data[0][0] == _data[1][0]:
+                    if abs(_data[0][0] - _data[1][0]) < chk_threshold:
                         _matches += 1
 
             metrics[key].append((_matches, _potential))
@@ -152,6 +181,13 @@ class Command(BaseCommand):
             metrics[key].append(deltas)
             metrics[key].append(len(value))
 
+        if export_csv:
+            _fields = (
+              'UserID', 'Ref', 'Chk', 'Bad', 'Count'
+            )
+            _header = ','.join(_fields)
+            print(_header)
+
         for key, value in metrics.items():
             metric1 = value[0][1] - value[0][0]
             metric2 = value[1][0] / value[1][1] if value[1][1] else 0
@@ -165,8 +201,16 @@ class Command(BaseCommand):
             except ImportError:
                 pass
 
-            print("{0}:\t{1:2d}\t{2:.2f}\t{3:f}\t{4:3d}".format(
-              key, metric1, metric2, metric3, value[3])
-            )
+            if not export_csv:
+                print("{0}\t{1:2d}\t{2:.2f}\t{3:f}\t{4:3d}".format(
+                  key, metric1, metric2, metric3, value[3])
+                )
 
-        print('\nExcluded IDs: {0}\n'.format(', '.join(exclude_ids)))
+            else:
+                _data = (key, str(metric1), str(metric2), str(metric3), str(value[3]))
+                _line = ','.join(_data)
+                _line = _line.replace('nan', '0.000000')
+                print(_line)
+
+        if not export_csv:
+            print('\nExcluded IDs: {0}\n'.format(', '.join(exclude_ids)))
