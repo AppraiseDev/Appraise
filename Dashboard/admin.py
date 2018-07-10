d165a24bc8e9e66ddf86df22a7b06316e48c855e
Christian Federmann <cfedermann@gmail.com>
2018-07-09 19:58:56 -0700

    PEP8 cleanup.

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,6 +1,7 @@
+# pylint: disable=import-error
 from django.contrib import admin
 
-from .models import UserInviteToken, TimedKeyValueData
+from Dashboard.models import UserInviteToken, TimedKeyValueData
 
 admin.site.register(UserInviteToken)
 admin.site.register(TimedKeyValueData)
