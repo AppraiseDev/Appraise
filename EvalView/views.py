d0d9607b348701907eca8736a0b645f215ffb6c1
Christian Federmann <cfedermann@gmail.com>
2018-07-09 19:05:06 -0700

    PEP8 cleanup.

--- EvalView/views.py
+++ EvalView/views.py
@@ -1,16 +1,22 @@
+from datetime import datetime
 import logging
 
-from datetime import datetime
-from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 from django.utils.timezone import utc
-from django.views import generic
 
+from Appraise.settings import (
+    LOG_LEVEL,
+    LOG_HANDLER,
+    BASE_CONTEXT,
+)
 from Campaign.models import Campaign
-from EvalData.models import DirectAssessmentTask, DirectAssessmentResult, \
-  TextPair, seconds_to_timedelta, MultiModalAssessmentTask, \
-  MultiModalAssessmentResult, WorkAgenda, TaskAgenda
-from Appraise.settings import LOG_LEVEL, LOG_HANDLER, STATIC_URL, BASE_CONTEXT
+from EvalData.models import (
+    DirectAssessmentTask, DirectAssessmentResult,
+    MultiModalAssessmentTask, MultiModalAssessmentResult,
+    TaskAgenda
+)
+
 
 # Setup logging support.
 logging.basicConfig(level=LOG_LEVEL)
@@ -30,13 +36,14 @@ def direct_assessment(request, code=None, campaign_name=None):
     if campaign_name:
         campaign = Campaign.objects.filter(campaignName=campaign_name)
         if not campaign.exists():
-            LOGGER.info('No campaign named "{0}" exists, redirecting to dashboard')
+            _msg = 'No campaign named "%s" exists, redirecting to dashboard'
+            LOGGER.info(_msg, campaign_name)
             return redirect('dashboard')
 
         campaign = campaign[0]
 
-    LOGGER.info('Rendering direct assessment view for user "{0}".'.format(
-      request.user.username or "Anonymous"))
+    LOGGER.info('Rendering direct assessment view for user "%s".',
+        request.user.username or "Anonymous")
 
     current_task = None
 
@@ -52,7 +59,7 @@ def direct_assessment(request, code=None, campaign_name=None):
 
     for agenda in agendas:
         modified = False
-        LOGGER.info('Identified work agenda {0}'.format(agenda))
+        LOGGER.info('Identified work agenda %s', agenda)
         for serialized_open_task in agenda._open_tasks.all():
             open_task = serialized_open_task.get_object_instance()
             if open_task.next_item_for_user(request.user) is not None:
@@ -69,7 +76,7 @@ def direct_assessment(request, code=None, campaign_name=None):
 
     if not current_task and agendas.count() > 0:
         LOGGER.info('Work agendas completed, redirecting to dashboard')
-        LOGGER.info('- code={0}, campaign={1}'.format(code, campaign))
+        LOGGER.info('- code=%s, campaign=%s', code, campaign)
         return redirect('dashboard')
 
     # If language code has been given, find a free task and assign to user.
@@ -79,13 +86,13 @@ def direct_assessment(request, code=None, campaign_name=None):
     if not current_task:
         if code is None or campaign is None:
             LOGGER.info('No current task detected, redirecting to dashboard')
-            LOGGER.info('- code={0}, campaign={1}'.format(code, campaign))
+            LOGGER.info('- code=%s, campaign=%s', code, campaign)
             return redirect('dashboard')
 
-        LOGGER.info('Identifying next task for code "{0}", campaign="{1}"' \
-          .format(code, campaign))
-        next_task = DirectAssessmentTask \
-          .get_next_free_task_for_language(code, campaign, request.user)
+        LOGGER.info('Identifying next task for code "%s", campaign="%s"',
+          code, campaign)
+        next_task = DirectAssessmentTask.get_next_free_task_for_language(
+            code, campaign, request.user)
 
         if next_task is None:
             LOGGER.info('No next task detected, redirecting to dashboard')
@@ -101,7 +108,8 @@ def direct_assessment(request, code=None, campaign_name=None):
             campaign = current_task.campaign
 
         elif campaign.campaignName != current_task.campaign.campaignName:
-            LOGGER.info('Incompatible campaign specified, using item campaign instead!')
+            _msg = 'Incompatible campaign given, using item campaign instead!'
+            LOGGER.info(_msg)
             campaign = current_task.campaign
 
     t2 = datetime.now()
@@ -111,20 +119,19 @@ def direct_assessment(request, code=None, campaign_name=None):
         task_id = request.POST.get('task_id', None)
         start_timestamp = request.POST.get('start_timestamp', None)
         end_timestamp = request.POST.get('end_timestamp', None)
-        LOGGER.info('score={0}, item_id={1}'.format(score, item_id))
+        LOGGER.info('score=%s, item_id=%s', score, item_id)
         if score and item_id and start_timestamp and end_timestamp:
             duration = float(end_timestamp) - float(start_timestamp)
             LOGGER.debug(float(start_timestamp))
             LOGGER.debug(float(end_timestamp))
-            LOGGER.info('start={0}, end={1}, duration={2}'.format(start_timestamp, end_timestamp, duration))
+            LOGGER.info('start=%s, end=%s, duration=%s',
+                start_timestamp, end_timestamp, duration)
 
             current_item = current_task.next_item_for_user(request.user)
-            if current_item.itemID != int(item_id) \
-              or current_item.id != int(task_id):
-                LOGGER.debug(
-                  'Item ID {0} does not match current item {1}, will ' \
-                  'not save result!'.format(item_id, current_item.itemID)
-                )
+            if (current_item.itemID != int(item_id) or
+                current_item.id != int(task_id)):
+                _msg = 'Item ID %s does not match item %s, will not save!'
+                LOGGER.debug(_msg, item_id, current_item.itemID)
 
             else:
                 utc_now = datetime.utcnow().replace(tzinfo=utc)
@@ -144,18 +151,16 @@ def direct_assessment(request, code=None, campaign_name=None):
 
     t3 = datetime.now()
 
-    current_item, completed_items = current_task.next_item_for_user(request.user, return_completed_items=True)
+    current_item, completed_items = current_task.next_item_for_user(
+        request.user, return_completed_items=True)
     if not current_item:
         LOGGER.info('No current item detected, redirecting to dashboard')
         return redirect('dashboard')
 
     # completed_items_check = current_task.completed_items_for_user(request.user)
     completed_blocks = int(completed_items / 10)
-    LOGGER.info(
-      'completed_items={0}, completed_blocks={1}'.format(
-        completed_items, completed_blocks
-      )
-    )
+    _msg = 'completed_items=%s, completed_blocks=%s'
+    LOGGER.info(_msg, completed_items, completed_blocks)
 
     source_language = current_task.marketSourceLanguage()
     target_language = current_task.marketTargetLanguage()
@@ -195,13 +200,14 @@ def multimodal_assessment(request, code=None, campaign_name=None):
     if campaign_name:
         campaign = Campaign.objects.filter(campaignName=campaign_name)
         if not campaign.exists():
-            LOGGER.info('No campaign named "{0}" exists, redirecting to dashboard')
+            _msg = 'No campaign named "%s" exists, redirecting to dashboard'
+            LOGGER.info(_msg, campaign_name)
             return redirect('dashboard')
 
         campaign = campaign[0]
 
-    LOGGER.info('Rendering multimodal assessment view for user "{0}".'.format(
-      request.user.username or "Anonymous"))
+    LOGGER.info('Rendering multimodal assessment view for user "%s".',
+        request.user.username or "Anonymous")
 
     current_task = None
 
@@ -217,7 +223,7 @@ def multimodal_assessment(request, code=None, campaign_name=None):
 
     for agenda in agendas:
         modified = False
-        LOGGER.info('Identified work agenda {0}'.format(agenda))
+        LOGGER.info('Identified work agenda %s', agenda)
         for serialized_open_task in agenda._open_tasks.all():
             open_task = serialized_open_task.get_object_instance()
             if open_task.next_item_for_user(request.user) is not None:
@@ -234,23 +240,24 @@ def multimodal_assessment(request, code=None, campaign_name=None):
 
     if not current_task and agendas.count() > 0:
         LOGGER.info('Work agendas completed, redirecting to dashboard')
-        LOGGER.info('- code={0}, campaign={1}'.format(code, campaign))
+        LOGGER.info('- code=%s, campaign=%s', code, campaign)
         return redirect('dashboard')
 
     # If language code has been given, find a free task and assign to user.
     if not current_task:
-        current_task = DirectAssessmentTask.get_task_for_user(user=request.user)
+        current_task = DirectAssessmentTask.get_task_for_user(
+            user=request.user)
 
     if not current_task:
         if code is None or campaign is None:
             LOGGER.info('No current task detected, redirecting to dashboard')
-            LOGGER.info('- code={0}, campaign={1}'.format(code, campaign))
+            LOGGER.info('- code=%s, campaign=%s', code, campaign)
             return redirect('dashboard')
 
-        LOGGER.info('Identifying next task for code "{0}", campaign="{1}"' \
-          .format(code, campaign))
-        next_task = MultiModalAssessmentTask \
-          .get_next_free_task_for_language(code, campaign, request.user)
+        _msg = 'Identifying next task for code "%s", campaign="%s"'
+        LOGGER.info(_msg, code, campaign)
+        next_task = MultiModalAssessmentTask.get_next_free_task_for_language(
+            code, campaign, request.user)
 
         if next_task is None:
             LOGGER.info('No next task detected, redirecting to dashboard')
@@ -266,7 +273,8 @@ def multimodal_assessment(request, code=None, campaign_name=None):
             campaign = current_task.campaign
 
         elif campaign.campaignName != current_task.campaign.campaignName:
-            LOGGER.info('Incompatible campaign specified, using item campaign instead!')
+            _msg = 'Incompatible campaign given, using item campaign instead!'
+            LOGGER.info(_msg)
             campaign = current_task.campaign
 
     t2 = datetime.now()
@@ -276,20 +284,19 @@ def multimodal_assessment(request, code=None, campaign_name=None):
         task_id = request.POST.get('task_id', None)
         start_timestamp = request.POST.get('start_timestamp', None)
         end_timestamp = request.POST.get('end_timestamp', None)
-        LOGGER.info('score={0}, item_id={1}'.format(score, item_id))
+        LOGGER.info('score=%s, item_id=%s', score, item_id)
         if score and item_id and start_timestamp and end_timestamp:
             duration = float(end_timestamp) - float(start_timestamp)
             LOGGER.debug(float(start_timestamp))
             LOGGER.debug(float(end_timestamp))
-            LOGGER.info('start={0}, end={1}, duration={2}'.format(start_timestamp, end_timestamp, duration))
+            LOGGER.info('start=%s, end=%s, duration=%s',
+                start_timestamp, end_timestamp, duration)
 
             current_item = current_task.next_item_for_user(request.user)
-            if current_item.itemID != int(item_id) \
-              or current_item.id != int(task_id):
-                LOGGER.debug(
-                  'Item ID {0} does not match current item {1}, will ' \
-                  'not save result!'.format(item_id, current_item.itemID)
-                )
+            if (current_item.itemID != int(item_id) or
+                current_item.id != int(task_id)):
+                _msg = 'Item ID %s does not match  item %s, will not save!'
+                LOGGER.debug(_msg, item_id, current_item.itemID)
 
             else:
                 utc_now = datetime.utcnow().replace(tzinfo=utc)
@@ -309,18 +316,16 @@ def multimodal_assessment(request, code=None, campaign_name=None):
 
     t3 = datetime.now()
 
-    current_item, completed_items = current_task.next_item_for_user(request.user, return_completed_items=True)
+    current_item, completed_items = current_task.next_item_for_user(
+        request.user, return_completed_items=True)
     if not current_item:
         LOGGER.info('No current item detected, redirecting to dashboard')
         return redirect('dashboard')
 
     # completed_items_check = current_task.completed_items_for_user(request.user)
     completed_blocks = int(completed_items / 10)
-    LOGGER.info(
-      'completed_items={0}, completed_blocks={1}'.format(
-        completed_items, completed_blocks
-      )
-    )
+    _msg = 'completed_items=%s, completed_blocks=%s'
+    LOGGER.info(_msg, completed_items, completed_blocks)
 
     source_language = current_task.marketSourceLanguage()
     target_language = current_task.marketTargetLanguage()
