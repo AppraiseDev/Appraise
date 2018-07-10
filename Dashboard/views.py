44841f0b517d71bb713bd5b3b9f324aedd1ee6e2
Christian Federmann <cfedermann@gmail.com>
2018-07-09 20:23:51 -0700

    PEP8 cleanup.

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,22 +1,25 @@
 # pylint: disable=C0330
-import logging
-
-from collections import defaultdict
 from datetime import datetime
 from hashlib import md5
+import logging
+
+# pylint: disable=import-error
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AdminPasswordChangeForm
-from django.contrib.auth.views import password_change
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.shortcuts import render, reverse, redirect, render_to_response
+from django.shortcuts import render, redirect, render_to_response
 
-from Appraise.settings import LOG_LEVEL, LOG_HANDLER, STATIC_URL, BASE_CONTEXT
-from EvalData.models import DirectAssessmentTask, DirectAssessmentResult
-from EvalData.models import MultiModalAssessmentTask, MultiModalAssessmentResult
-from EvalData.models import WorkAgenda, TaskAgenda
-from .models import UserInviteToken, LANGUAGE_CODES_AND_NAMES
+from Appraise.settings import (
+    LOG_LEVEL,
+    LOG_HANDLER,
+    BASE_CONTEXT,
+)
+from Dashboard.models import UserInviteToken, LANGUAGE_CODES_AND_NAMES
+from EvalData.models import (
+    DirectAssessmentTask, DirectAssessmentResult,
+    MultiModalAssessmentTask, MultiModalAssessmentResult,
+    TaskAgenda
+)
 
 
 # Setup logging support.
@@ -29,17 +32,25 @@ HITS_REQUIRED_BEFORE_ENGLISH_ALLOWED = 5
 
 # HTTP error handlers supporting COMMIT_TAG.
 def _page_not_found(request, template_name='404.html'):
-    """Custom HTTP 404 handler that preserves URL_PREFIX."""
-    LOGGER.info('Rendering HTTP 404 for user "{0}". Request.path={1}'.format(
-      request.user.username or "Anonymous", request.path))
+    """
+    Custom HTTP 404 handler that preserves URL_PREFIX.
+    """
+    del template_name  # Unused.
+
+    LOGGER.info('Rendering HTTP 404 for user "%s". Request.path=%s',
+        request.user.username or "Anonymous", request.path)
 
     return render_to_response('Dashboard/404.html', BASE_CONTEXT)
 
 
 def _server_error(request, template_name='500.html'):
-    """Custom HTTP 500 handler that preserves URL_PREFIX."""
-    LOGGER.info('Rendering HTTP 500 for user "{0}". Request.path={1}'.format(
-      request.user.username or "Anonymous", request.path))
+    """
+    Custom HTTP 500 handler that preserves URL_PREFIX.
+    """
+    del template_name  # Unused.
+
+    LOGGER.info('Rendering HTTP 500 for user "%s". Request.path=%s',
+        request.user.username or "Anonymous", request.path)
 
     return render_to_response('Dashboard/500.html', BASE_CONTEXT)
 
@@ -52,8 +63,8 @@ def sso_login(request, username, password):
         user = authenticate(username=username, password=password)
         login(request, user)
 
-    LOGGER.info('Rendering SSO login view for user "{0}".'.format(
-      request.user.username or "Anonymous"))
+    LOGGER.info('Rendering SSO login view for user "%s".',
+        request.user.username or "Anonymous")
 
     return redirect('dashboard')
 
@@ -61,8 +72,8 @@ def frontpage(request, extra_context=None):
     """
     Appraise front page.
     """
-    LOGGER.info('Rendering frontpage view for user "{0}".'.format(
-      request.user.username or "Anonymous"))
+    LOGGER.info('Rendering frontpage view for user "%s".',
+        request.user.username or "Anonymous")
 
     context = {
       'active_page': 'frontpage'
@@ -237,7 +248,7 @@ def update_profile(request):
 
     # Determine user target languages
     for group in request.user.groups.all():
-        if group.name.lower() in [x.lower() for x in LANGUAGE_CODES_AND_NAMES.keys()]:
+        if group.name.lower() in [x.lower() for x in LANGUAGE_CODES_AND_NAMES]:
             languages.add(group.name.lower())
 
     context = {
@@ -257,7 +268,7 @@ def dashboard(request):
     """
     Appraise dashboard page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'dashboard'
@@ -279,10 +290,8 @@ def dashboard(request):
         code = current_task.marketTargetLanguageCode()
         print(request.user.groups.all())
         if code not in request.user.groups.values_list('name', flat=True):
-            LOGGER.info('Language {0} not in user languages for user {1}. ' \
-              'Giving up task {2}'.format(code, request.user.username,
-              current_task)
-            )
+            _msg = 'Language %s not specified for user %s. Giving up task %s'
+            LOGGER.info(_msg, code, request.user.username, current_task)
 
             current_task.assignedTo.remove(request.user)
             current_task = None
@@ -295,15 +304,13 @@ def dashboard(request):
         code = current_task.marketTargetLanguageCode()
         print(request.user.groups.all())
         if code not in request.user.groups.values_list('name', flat=True):
-            LOGGER.info('Language {0} not in user languages for user {1}. ' \
-              'Giving up task {2}'.format(code, request.user.username,
-              current_task)
-            )
+            _msg = 'Language %s not specified for user %s. Giving up task %s'
+            LOGGER.info(_msg, code, request.user.username, current_task)
 
             current_task.assignedTo.remove(request.user)
             current_task = None
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
 
     # If there is no current task, check if user is done with work agenda.
     work_completed = False
@@ -313,17 +320,16 @@ def dashboard(request):
         )
 
         for agenda in agendas:
-            LOGGER.info('Identified work agenda {0}'.format(agenda))
-            for serialized_open_task in agenda._open_tasks.all():
+            LOGGER.info('Identified work agenda %s', agenda)
+            for serialized_open_task in agenda.serialized_open_tasks():
                 open_task = serialized_open_task.get_object_instance()
                 if open_task.next_item_for_user(request.user) is not None:
                     current_task = open_task
                     campaign = agenda.campaign
-                    LOGGER.info('Current task type: {0}'.format(
-                        open_task.__class__.__name__))
+                    LOGGER.info('Current task type: %s',
+                        open_task.__class__.__name__)
                 else:
-                    agenda._completed_tasks.add(serialized_open_task)
-                    agenda._open_tasks.remove(serialized_open_task)
+                    agenda.completed_open_task(serialized_open_task)
             agenda.save()
 
         if not current_task and agendas.count() > 0:
@@ -359,10 +365,14 @@ def dashboard(request):
 
             for code in languages:
                 next_task_available = None
+
                 if not is_multi_modal_campaign:
-                    next_task_available = DirectAssessmentTask.get_next_free_task_for_language(code, campaign, request.user)
+                    _cls = DirectAssessmentTask
                 else:
-                    next_task_available = MultiModalAssessmentTask.get_next_free_task_for_language(code, campaign, request.user)
+                    _cls = MultiModalAssessmentTask
+
+                next_task_available = _cls.get_next_free_task_for_language(
+                    code, campaign, request.user)
 
                 if not next_task_available:
                     if not is_multi_modal_campaign:
@@ -370,11 +380,14 @@ def dashboard(request):
                     else:
                         multimodal_languages[campaign.campaignName].remove(code)
 
-            print("campaign = {0} languages = {1}".format(
-              campaign.campaignName, campaign_languages[campaign.campaignName] if not is_multi_modal_campaign else multimodal_languages[campaign.campaignName]
-            ))
+            _languages = campaign_languages
+            if is_multi_modal_campaign:
+                _languages = multimodal_languages
 
-    t3 = datetime.now()
+            print("campaign = {0} languages = {1}".format(
+              campaign.campaignName, _languages[campaign.campaignName]))
+
+    _t3 = datetime.now()
 
     duration = DirectAssessmentResult.get_time_for_user(request.user)
     days = duration.days
@@ -388,7 +401,7 @@ def dashboard(request):
     minutes += int(((duration.total_seconds() - (days * 86400)) % 3600) / 60)
     seconds += int((duration.total_seconds() - (days * 86400)) % 60)
 
-    t4 = datetime.now()
+    _t4 = datetime.now()
 
     all_languages = []
     for key, values in campaign_languages.items():
@@ -420,7 +433,7 @@ def dashboard(request):
       'current_type': 'direct' if not is_multi_modal_campaign else 'multimodal',
       'languages': all_languages,
       'multimodal': mmt_languages,
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
       'work_completed': work_completed,
     })
@@ -433,29 +446,30 @@ def group_status(request):
     """
     Appraise group status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'group-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     group_data = DirectAssessmentResult.compute_accurate_group_status()
-    t3 = datetime.now()
+    _t3 = datetime.now()
 
-    group_status = []
+    _group_status = []
     for group in group_data:
-        group_status.append((group, group_data[group][0], group_data[group][1]))
+        _group_status.append(
+            (group, group_data[group][0], group_data[group][1]))
 
-    sorted_status = sorted(group_status, key=lambda x: x[1], reverse=True)
-    t4 = datetime.now()
+    sorted_status = sorted(_group_status, key=lambda x: x[1], reverse=True)
+    _t4 = datetime.now()
 
     context.update({
       'group_status': list(sorted_status),
-      'sum_completed': sum([x[1] for x in group_status]),
-      'sum_total': sum([x[2] for x in group_status]),
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'sum_completed': sum([x[1] for x in _group_status]),
+      'sum_total': sum([x[2] for x in _group_status]),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
@@ -467,29 +481,30 @@ def multimodal_status(request):
     """
     Appraise group status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'group-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     group_data = MultiModalAssessmentResult.compute_accurate_group_status()
-    t3 = datetime.now()
+    _t3 = datetime.now()
 
-    group_status = []
+    _group_status = []
     for group in group_data:
-        group_status.append((group, group_data[group][0], group_data[group][1]))
+        _group_status.append(
+            (group, group_data[group][0], group_data[group][1]))
 
-    sorted_status = sorted(group_status, key=lambda x: x[1], reverse=True)
-    t4 = datetime.now()
+    sorted_status = sorted(_group_status, key=lambda x: x[1], reverse=True)
+    _t4 = datetime.now()
 
     context.update({
       'group_status': list(sorted_status),
-      'sum_completed': sum([x[1] for x in group_status]),
-      'sum_total': sum([x[2] for x in group_status]),
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'sum_completed': sum([x[1] for x in _group_status]),
+      'sum_total': sum([x[2] for x in _group_status]),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
@@ -501,16 +516,16 @@ def system_status(request):
     """
     Appraise system status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'system-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     system_data = DirectAssessmentResult.get_system_status(sort_index=1)
-    t3 = datetime.now()
+    _t3 = datetime.now()
     sorted_status = []
     total_completed = 0
     for code in system_data:
@@ -521,11 +536,11 @@ def system_status(request):
             sorted_status.append((code, data[0], data[1]))
             total_completed += data[1]
 
-    t4 = datetime.now()
+    _t4 = datetime.now()
     context.update({
       'system_status': sorted_status,
       'total_completed': total_completed,
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
@@ -537,16 +552,16 @@ def multimodal_systems(request):
     """
     Appraise system status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'system-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     system_data = MultiModalAssessmentResult.get_system_status(sort_index=1)
-    t3 = datetime.now()
+    _t3 = datetime.now()
     sorted_status = []
     total_completed = 0
     for code in system_data:
@@ -557,11 +572,11 @@ def multimodal_systems(request):
             sorted_status.append((code, data[0], data[1]))
             total_completed += data[1]
 
-    t4 = datetime.now()
+    _t4 = datetime.now()
     context.update({
       'system_status': sorted_status,
       'total_completed': total_completed,
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
@@ -573,29 +588,30 @@ def metrics_status(request):
     """
     Appraise system status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'system-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     task_data = DirectAssessmentTask.objects.filter(id__in=[x+5427 for x in range(48)])
-    t3 = datetime.now()
+    _t3 = datetime.now()
     task_status = []
-    for t in task_data.order_by('id'):
-        source_language = t.items.first().metadata.market.sourceLanguageCode
-        target_language = t.items.first().metadata.market.targetLanguageCode
-        annotators = t.assignedTo.count()
-        results = t.evaldata_directassessmentresult_task.count()
+    for task in task_data.order_by('id'):
+        source_language = task.items.first().metadata.market.sourceLanguageCode
+        target_language = task.items.first().metadata.market.targetLanguageCode
+        annotators = task.assignedTo.count()
+        results = task.evaldata_directassessmentresult_task.count()
         task_status.append((
-          t.id, source_language, target_language, annotators, round(100*annotators/15.0), results, round(100*results/(15*70.0))
+          task.id, source_language, target_language, annotators,
+          round(100*annotators/15.0), results, round(100*results/(15*70.0))
         ))
-    t4 = datetime.now()
+    _t4 = datetime.now()
     context.update({
       'task_status': task_status,
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
@@ -607,29 +623,30 @@ def fe17_status(request):
     """
     Appraise system status page.
     """
-    t1 = datetime.now()
+    _t1 = datetime.now()
 
     context = {
       'active_page': 'system-status'
     }
     context.update(BASE_CONTEXT)
 
-    t2 = datetime.now()
+    _t2 = datetime.now()
     task_data = DirectAssessmentTask.objects.filter(id__gte=37)
-    t3 = datetime.now()
+    _t3 = datetime.now()
     task_status = []
-    for t in task_data.order_by('id'):
-        source_language = t.items.first().metadata.market.sourceLanguageCode
-        target_language = t.items.first().metadata.market.targetLanguageCode
-        annotators = t.assignedTo.count()
-        results = t.evaldata_directassessmentresult_task.count()
+    for task in task_data.order_by('id'):
+        source_language = task.items.first().metadata.market.sourceLanguageCode
+        target_language = task.items.first().metadata.market.targetLanguageCode
+        annotators = task.assignedTo.count()
+        results = task.evaldata_directassessmentresult_task.count()
         task_status.append((
-          t.id, source_language, target_language, annotators, round(100*annotators/4.0), results, round(100*results/(4*100.0))
+            task.id, source_language, target_language, annotators,
+            round(100*annotators/4.0), results, round(100*results/(4*100.0))
         ))
-    t4 = datetime.now()
+    _t4 = datetime.now()
     context.update({
       'task_status': task_status,
-      'debug_times': (t2-t1, t3-t2, t4-t3, t4-t1),
+      'debug_times': (_t2-_t1, _t3-_t2, _t4-_t3, _t4-_t1),
       'template_debug': 'debug' in request.GET,
     })
 
