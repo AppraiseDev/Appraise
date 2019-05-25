10971d387950d4e692ed712f4415383e2a0c36fc
Christian Federmann <cfedermann@gmail.com>
2019-05-24 21:53:52 -0700

    Cleanup black -l 75 -S Dashboard\management\commands\CreateInviteTokens.py

--- Dashboard/management/commands/CreateInviteTokens.py
+++ Dashboard/management/commands/CreateInviteTokens.py
@@ -1,6 +1,9 @@
 """
 Appraise evaluation framework
+
+See LICENSE for usage details
 """
+# pylint: disable=C0103
 from collections import defaultdict
 from csv import DictWriter, DictReader
 from datetime import datetime
@@ -25,20 +28,24 @@ class Command(BaseCommand):
     # pylint: disable=C0330,no-self-use
     def add_arguments(self, parser):
         parser.add_argument(
-          'group_name', type=str,
-          help='Name of the group the invite tokens belong to'
+            'group_name',
+            type=str,
+            help='Name of the group the invite tokens belong to',
         )
         parser.add_argument(
-          'number_of_tokens', type=int,
-          help='Number of tokens to create'
+            'number_of_tokens', type=int, help='Number of tokens to create'
         )
         parser.add_argument(
-          '--create-group', type=bool, default=False,
-          help='Create group if it does not exist yet'
+            '--create-group',
+            type=bool,
+            default=False,
+            help='Create group if it does not exist yet',
         )
         parser.add_argument(
-          '--output-file', type=str, default=None,
-          help='Output file path'
+            '--output-file',
+            type=str,
+            default=None,
+            help='Output file path',
         )
 
     def handle(self, *args, **options):
@@ -62,29 +69,32 @@ class Command(BaseCommand):
         if not Group.objects.filter(name=group_name).exists():
             if not create_group:
                 _msg = '{0}Specified group does not exist: {1}\n'.format(
-                  WARNING_MSG, group_name
+                    WARNING_MSG, group_name
                 )
                 self.stdout.write(_msg)
-                self.stdout.write('      You can use --create-group to create it.')
+                self.stdout.write(
+                    '      You can use --create-group to create it.'
+                )
                 self.stdout.write('\n[FAIL]\n\n')
                 return
 
-            else:
-                new_group = Group(name=group_name)
-                new_group.save()
+            new_group = Group(name=group_name)
+            new_group.save()
 
         if number_of_tokens < 1 or number_of_tokens > 50:
             _msg = '{0}Specified number of tokens is insane: {1}'.format(
-              WARNING_MSG, number_of_tokens
+                WARNING_MSG, number_of_tokens
             )
             self.stdout.write(_msg)
-            self.stdout.write('      You can request creation of up to 50 tokens.')
+            self.stdout.write(
+                '      You can request creation of up to 50 tokens.'
+            )
             self.stdout.write('\n[FAIL]\n\n')
             return
 
         group_password = '{0}{1}'.format(
-          group_name[:2].upper(),
-          md5(group_name.encode('utf-8')).hexdigest()[:8]
+            group_name[:2].upper(),
+            md5(group_name.encode('utf-8')).hexdigest()[:8],
         )
 
         date_created = datetime.utcnow().isoformat()
@@ -101,18 +111,24 @@ class Command(BaseCommand):
             new_token.save()
 
             tokens.append(new_token.token)
-            self.stdout.write('           token: {0}'.format(new_token.token))
+            self.stdout.write(
+                '           token: {0}'.format(new_token.token)
+            )
 
         if output_file is not None:
             csv_fields = ('key', 'value')
             csv_data = defaultdict(set)
 
             if exists(output_file):
-                with open(output_file, mode='r', encoding='utf-8') as in_file:
+                with open(
+                    output_file, mode='r', encoding='utf-8'
+                ) as in_file:
                     csv_reader = DictReader(in_file, csv_fields)
                     for csv_line in csv_reader:
-                        if not 'key' in csv_line.keys() or \
-                          not 'value' in csv_line.keys():
+                        if (
+                            not 'key' in csv_line.keys()
+                            or not 'value' in csv_line.keys()
+                        ):
                             continue
 
                         csv_data[csv_line['key']].add(csv_line['value'])
@@ -124,25 +140,30 @@ class Command(BaseCommand):
                 # 3. 'group_password' --> set({group_password})
                 invalid_csv_file = False
 
-                if not 'key' in csv_data.keys() or \
-                  csv_data['key'] != {'value'}:
+                if not 'key' in csv_data.keys() or csv_data['key'] != {
+                    'value'
+                }:
                     invalid_csv_file = True
 
-                if not 'group_name' in csv_data.keys() or \
-                  csv_data['group_name'] != {group_name}:
+                if not 'group_name' in csv_data.keys() or csv_data[
+                    'group_name'
+                ] != {group_name}:
                     invalid_csv_file = True
 
-                if not 'group_password' in csv_data.keys() or \
-                  csv_data['group_password'] != {group_password}:
+                if not 'group_password' in csv_data.keys() or csv_data[
+                    'group_password'
+                ] != {group_password}:
                     invalid_csv_file = True
 
                 if invalid_csv_file:
                     _msg = '\n{0}Incompatible output file given: {1}'.format(
-                      WARNING_MSG, abspath(output_file)
+                        WARNING_MSG, abspath(output_file)
                     )
                     self.stdout.write(_msg)
-                    self.stdout.write('      File needs (key, value) ' \
-                      'headers and group name and password have to match.')
+                    self.stdout.write(
+                        '      File needs (key, value) '
+                        'headers and group name and password have to match.'
+                    )
                     self.stdout.write('\n[FAIL]\n\n')
                     return
 
@@ -156,10 +177,10 @@ class Command(BaseCommand):
                 csv_writer = DictWriter(out_file, csv_fields)
 
                 csv_rows = [
-                  {'key': 'group_name', 'value': group_name},
-                  {'key': 'group_password', 'value': group_password},
-                  {'key': 'number_of_tokens', 'value': number_of_tokens},
-                  {'key': 'date_created', 'value': date_created}
+                    {'key': 'group_name', 'value': group_name},
+                    {'key': 'group_password', 'value': group_password},
+                    {'key': 'number_of_tokens', 'value': number_of_tokens},
+                    {'key': 'date_created', 'value': date_created},
                 ]
 
                 for token in tokens:
