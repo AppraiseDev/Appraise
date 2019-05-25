a72696eab612e945dfbb9bad9ebcbd9007e2dcc9
Christian Federmann <cfedermann@gmail.com>
2019-05-24 21:32:20 -0700

    Minor cleanup.

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,8 @@
+"""
+Appraise evaluation framework
+
+See LICENSE for usage details
+"""
 # pylint: disable=import-error
 from django.contrib import admin
 
--- Dashboard/tests.py
+++ Dashboard/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+"""
+Appraise evaluation framework
 
-# Create your tests here.
+See LICENSE for usage details
+"""
+# pylint: disable=unused-import
+from django.db import models
+
+# Deliberately left empty.
