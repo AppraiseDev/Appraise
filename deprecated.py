5a02557dd8ffc5196f4135da0f217d5740c1fad5
Christian Federmann <cfedermann@gmail.com>
2019-05-27 16:50:40 -0700

    Added lightweight deprecation mechanism.

--- deprecated.py
+++ deprecated.py
@@ -2,8 +2,34 @@
 Appraise evaluation framework
 
 See LICENSE for usage details
+
+Keeps a registry of deprecated methods.
+
+Use @add_deprecated_method decorator to mark method as deprecated. This
+needs to be placed closest relative to the deprecated method for now.
+
+Use get_deprecated_methods() to retrieve set of deprecated methods.
 """
-from django.contrib.auth.models import User
+from typing import Set
+
+
+_DEPRECATED_METHOD_REGISTRY: Set[str] = set()
+
+
+def add_deprecated_method(func):
+    """
+    Add deprecated method to registry.
+    """
+    _DEPRECATED_METHOD_REGISTRY.add(func.__name__)
+    return func
+
+
+def get_deprecated_methods():
+    """
+    Get deprecated methods from registry.
+    """
+    return _DEPRECATED_METHOD_REGISTRY
+
 
 # pylint: disable=protected-access
 def reassign_tasks(cls, old_username, new_username):
@@ -13,6 +39,8 @@ def reassign_tasks(cls, old_username, new_username):
     Used to be @classmethod in EvalData.models.TaskAgenda.
     Method has been deprecated on 5/27/2019.
     """
+    from django.contrib.auth.models import User
+
     old_user = User.objects.get(username=old_username)
     new_user = User.objects.get(username=new_username)
 
