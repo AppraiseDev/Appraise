c9d01a5911922643877d059cda22a603492e4d09
Christian Federmann <cfedermann@gmail.com>
2019-06-18 06:01:23 -0700

    Updated management command for campaign FY1993

--- Campaign/management/commands/InitCampaignFY1993.py
+++ Campaign/management/commands/InitCampaignFY1993.py
@@ -76,12 +76,12 @@ for code in EX_LANGUAGES + XE_LANGUAGES + XY_LANGUAGES:
 
 for ex_code in EX_LANGUAGES:
     TASKS_TO_ANNOTATORS[('eng', ex_code)] = _create_uniform_task_map(
-        5, 10, REDUNDANCY
+        10, 20, REDUNDANCY
     )
 
 for xe_code in XE_LANGUAGES:
     TASKS_TO_ANNOTATORS[(xe_code, 'eng')] = _create_uniform_task_map(
-        5, 10, REDUNDANCY
+        10, 20, REDUNDANCY
     )
 
 for xy_code in XY_LANGUAGES:
