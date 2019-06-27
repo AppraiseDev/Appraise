c01db1b3574aae08c9784584531b7205164fee08
Christian Federmann <chrife@microsoft.com>
2019-06-27 14:01:58 -0700

    Cleaned up EvalData\views.py

--- EvalData/views.py
+++ EvalData/views.py
@@ -4,13 +4,18 @@ Appraise evaluation framework
 See LICENSE for usage details
 """
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import (
+    login_required,
+    permission_required,
+)
 from django.http import HttpResponseRedirect
+
 # pylint: disable=unused-import
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from .models import TaskAgenda
+
 
 @login_required
 @permission_required('EvalData.reset_taskagenda')
@@ -31,4 +36,6 @@ def reset_taskagenda(request, agenda_id):
     _ret, _msg, _lvl = _obj.reset_taskagenda()
     messages.add_message(request, _lvl, _msg)
 
-    return HttpResponseRedirect(reverse('admin:EvalData_taskagenda_changelist'))
+    return HttpResponseRedirect(
+        reverse('admin:EvalData_taskagenda_changelist')
+    )
