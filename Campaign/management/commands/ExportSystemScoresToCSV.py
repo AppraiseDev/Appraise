e33f318ad719ff629c4b23b3772788a5f4d259ad
Christian Federmann <chrife@microsoft.com>
2019-06-27 10:38:23 -0700

    Cleaned up ExportSystemScoresToCSV management command

--- Campaign/management/commands/ExportSystemScoresToCSV.py
+++ Campaign/management/commands/ExportSystemScoresToCSV.py
@@ -3,9 +3,12 @@ from django.core.management.base import BaseCommand, CommandError
 
 from Campaign.models import Campaign
 from EvalData.models import (
-    DirectAssessmentResult, DirectAssessmentTask,
-    DirectAssessmentContextResult, DirectAssessmentContextTask,
-    MultiModalAssessmentResult, MultiModalAssessmentTask,
+    DirectAssessmentResult,
+    DirectAssessmentTask,
+    DirectAssessmentContextResult,
+    DirectAssessmentContextTask,
+    MultiModalAssessmentResult,
+    MultiModalAssessmentTask,
 )
 
 CAMPAIGN_TASK_TYPES = (
@@ -14,17 +17,20 @@ CAMPAIGN_TASK_TYPES = (
     (MultiModalAssessmentTask, MultiModalAssessmentResult),
 )
 
+
 class Command(BaseCommand):
     help = 'Exports system scores over all results to CSV format'
 
     def add_arguments(self, parser):
         parser.add_argument(
-          'campaign_name', type=str,
-          help='Name of the campaign you want to process data for'
+            'campaign_name',
+            type=str,
+            help='Name of the campaign you want to process data for',
         )
         parser.add_argument(
-          '--completed-only', action='store_true',
-          help='Include completed tasks only in the computation'
+            '--completed-only',
+            action='store_true',
+            help='Include completed tasks only in the computation',
         )
         # TODO: add argument to specify batch user
 
@@ -32,7 +38,8 @@ class Command(BaseCommand):
         # Identify Campaign instance for given name.
         try:
             campaign = Campaign.get_campaign_or_raise(
-                options['campaign_name'])
+                options['campaign_name']
+            )
 
         except LookupError as error:
             raise CommandError(error)
@@ -49,7 +56,8 @@ class Command(BaseCommand):
 
             if qs_obj and qs_obj.exists():
                 _scores = result_cls.get_system_data(
-                    campaign.id, extended_csv=True)
+                    campaign.id, extended_csv=True
+                )
                 system_scores.extend(_scores)
 
         for system_score in system_scores:
