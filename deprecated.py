1831e3bec7cd7b420a7d3590d298c13012f5120b
Christian Federmann <cfedermann@gmail.com>
2019-07-08 21:52:53 -0700

    Updated deprecated methods

--- deprecated.py
+++ deprecated.py
@@ -79,6 +79,44 @@ def fe17_status(request):
     return render(request, 'Dashboard/metrics-status.html', context)
 
 
+def group_status(request):
+    """
+    Appraise group status page.
+
+    Used to be @login_required method in Dashboard.views.
+    Method has been deprecated on 7/08/2019.
+    """
+    _t1 = datetime.now()
+
+    context = {'active_page': 'group-status'}
+    context.update(BASE_CONTEXT)
+
+    _t2 = datetime.now()
+    group_data = DirectAssessmentResult.compute_accurate_group_status()
+    _t3 = datetime.now()
+
+    _group_status = []
+    for group in group_data:
+        _group_status.append(
+            (group, group_data[group][0], group_data[group][1])
+        )
+
+    sorted_status = sorted(_group_status, key=lambda x: x[1], reverse=True)
+    _t4 = datetime.now()
+
+    context.update(
+        {
+            'group_status': list(sorted_status),
+            'sum_completed': sum([x[1] for x in _group_status]),
+            'sum_total': sum([x[2] for x in _group_status]),
+            'debug_times': (_t2 - _t1, _t3 - _t2, _t4 - _t3, _t4 - _t1),
+            'template_debug': 'debug' in request.GET,
+        }
+    )
+
+    return render(request, 'Dashboard/group-status.html', context)
+
+
 def metrics_status(request):
     """
     Appraise system status page.
@@ -167,6 +205,7 @@ def multimodal_status(request):
 
     return render(request, 'Dashboard/group-status.html', context)
 
+
 def multimodal_systems(request):
     """
     Appraise system status page.
@@ -237,3 +276,41 @@ def reassign_tasks(cls, old_username, new_username):
     new_tasks = list(new_agenda._open_tasks.all())
 
     return (old_tasks, new_tasks)
+
+
+def system_status(request):
+    """
+    Appraise system status page.
+
+    Used to be @login_required method in Dashboard.views.
+    Method has been deprecated on 7/08/2019.
+    """
+    _t1 = datetime.now()
+
+    context = {'active_page': 'system-status'}
+    context.update(BASE_CONTEXT)
+
+    _t2 = datetime.now()
+    system_data = DirectAssessmentResult.get_system_status(sort_index=1)
+    _t3 = datetime.now()
+    sorted_status = []
+    total_completed = 0
+    for code in system_data:
+        if not system_data[code]:
+            continue
+
+        for data in system_data[code]:
+            sorted_status.append((code, data[0], data[1]))
+            total_completed += data[1]
+
+    _t4 = datetime.now()
+    context.update(
+        {
+            'system_status': sorted_status,
+            'total_completed': total_completed,
+            'debug_times': (_t2 - _t1, _t3 - _t2, _t4 - _t3, _t4 - _t1),
+            'template_debug': 'debug' in request.GET,
+        }
+    )
+
+    return render(request, 'Dashboard/system-status.html', context)
