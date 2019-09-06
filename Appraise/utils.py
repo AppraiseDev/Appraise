8eaaa8fbfafac09be847cac53f3f3093bb8c9252
Christian Federmann <chrife@microsoft.com>
2019-09-06 14:23:43 -0700

    Added Appraise.utils with _get_logger method

---
+++ Appraise/utils.py
@@ -0,0 +1,17 @@
+"""
+Appraise evaluation framework
+
+See LICENSE for usage details
+"""
+import logging
+
+from Appraise.settings import LOG_LEVEL, LOG_HANDLER
+
+def _get_logger(name):
+    """
+    Initialises and returns named Django logger instance.
+    """
+    logging.basicConfig(level=LOG_LEVEL)
+    named_logger = logging.getLogger(name=name)
+    named_logger.addHandler(LOG_HANDLER)
+    return named_logger
