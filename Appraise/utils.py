2d5021862dde242eb66b00a1467cb5142daab0e8
Christian Federmann <chrife@microsoft.com>
2019-09-06 14:28:44 -0700

    Honor LOG_LEVEL set in LOG_HANDLER

--- Appraise/utils.py
+++ Appraise/utils.py
@@ -5,13 +5,12 @@ See LICENSE for usage details
 """
 import logging
 
-from Appraise.settings import LOG_LEVEL, LOG_HANDLER
+from Appraise.settings import LOG_HANDLER
 
 def _get_logger(name):
     """
     Initialises and returns named Django logger instance.
     """
-    logging.basicConfig(level=LOG_LEVEL)
     named_logger = logging.getLogger(name=name)
     named_logger.addHandler(LOG_HANDLER)
     return named_logger
