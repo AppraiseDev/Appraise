679cb95ad54b90b170480d4823910f0b107c6bba
Christian Federmann <cfedermann@gmail.com>
2021-08-06 20:25:34 +0000

    Explicitly set versions of validation dependencies

--- validate.sh
+++ validate.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
-pip install black mypy pylint-fail-under reorder-python-imports safety
-pip install -r requirements.txt
-
 export PYLINT_THRESHOLD=6
 export BLACK_LINE_MAXLEN=88
+
+pip install black==21.7b0 mypy==0.900 pylint==2.9.6 pylint-fail-under==0.3.0 reorder-python-imports==2.6.0 safety==1.10.3
+pip install -r requirements.txt
 
 black -S -l $BLACK_LINE_MAXLEN --check .
 find . -name "*.py" -not -path "./.venv/*" | xargs mypy
