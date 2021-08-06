1c21f491d6ec6ee080bb8521fd255477b4c39712
Christian Federmann <cfedermann@gmail.com>
2021-08-06 12:39:46 +0000

    Adopt black's default line width

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 
 export PYLINT_THRESHOLD=8
 
-black -S -l 75 --check .
+black -S --check .
 find . -name "*.py" -not -path "./.venv/*" | xargs mypy
 find . -name "*.py" -not -path "./.venv/*" | xargs pylint-fail-under --fail_under $PYLINT_THRESHOLD
 find . -name "*.py" -not -path "./.venv/*" | xargs reorder-python-imports --diff-only
