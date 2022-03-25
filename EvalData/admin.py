b58e262210f8190acbb4413775d78d3b091942af
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2022-03-25 12:31:38 +0000

    Clean imports reducing duplication

--- EvalData/admin.py
+++ EvalData/admin.py
@@ -12,29 +12,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.timezone import utc
 
-from .models import DataAssessmentResult
-from .models import DataAssessmentTask
-from .models import DirectAssessmentContextResult
-from .models import DirectAssessmentContextTask
-from .models import DirectAssessmentDocumentResult
-from .models import DirectAssessmentDocumentTask
-from .models import DirectAssessmentResult
-from .models import DirectAssessmentTask
-from .models import Market
-from .models import Metadata
-from .models import MultiModalAssessmentResult
-from .models import MultiModalAssessmentTask
-from .models import PairwiseAssessmentResult
-from .models import PairwiseAssessmentTask
-from .models import PairwiseAssessmentDocumentResult
-from .models import PairwiseAssessmentDocumentTask
-from .models import TaskAgenda
-from .models import TextPair
-from .models import TextPairWithContext
-from .models import TextPairWithImage
-from .models import TextSegment
-from .models import TextSegmentWithTwoTargets
-from .models import WorkAgenda
+from .models import *
 
 
 # TODO:chrife: find a way to use SELECT-based filtering widgets
