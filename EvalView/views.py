6a7ce9950e66df12c54c700eddf1e162f733e4b9
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2023-03-17 11:39:23 +0000

    Render <eos> tags as code

--- EvalView/views.py
+++ EvalView/views.py
@@ -2030,8 +2030,8 @@ def pairwise_assessment_document(request, code=None, campaign_name=None):
             'current_item': bool(item.id == current_item.id),
             'score1': result.score1 if result else -1,
             'score2': result.score2 if result else -1,
-            'candidate1_text': _candidate1_text,
-            'candidate2_text': _candidate2_text,
+            'candidate1_text': _candidate1_text.replace("&lt;eos&gt;", "<code>&lt;eos&gt;</code>"),
+            'candidate2_text': _candidate2_text.replace("&lt;eos&gt;", "<code>&lt;eos&gt;</code>"),
         }
         block_scores.append(item_scores)
 
@@ -2112,7 +2112,7 @@ def pairwise_assessment_document(request, code=None, campaign_name=None):
             '<u><b>paying special attention to document-level properties, '
             'such as consistency of formality and style, selection of translation terms, pronoun choice, '
             'and so on</b></u>, in addition to the usual correctness criteria. '
-            'Note that sentences in each paragraph were separated by the <i>&lt;eos&gt;</i> tags '
+            'Note that sentences in each paragraph were separated by the <code>&lt;eos&gt;</code> tags '
             'for convenience and this should not impact your assessment. '.format(
                 len(block_items) - 1,
                 source_language,
