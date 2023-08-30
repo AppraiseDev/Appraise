f56d02c471a00bcb9a35c8bec9cf6beb9ef3b213
Christian Federmann <cfedermann@gmail.com>
2023-08-30 06:24:59 -0700

    Update install-psql.sh
    Support PostgreSQL 14

--- install-psql.sh
+++ install-psql.sh
@@ -2,9 +2,9 @@
 which psql
 if [ "$?" -gt "0" ]; then
   echo "Not installed"
-  apt-get install -y curl ca-certificates gnupg lsb-core
-  curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-  sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  apt-get install -y curl ca-certificates gnupg apt-transport-https
+  curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /usr/share/keyrings/postgresql-keyring.gpg
+  sh -c 'echo "deb [signed-by=/usr/share/keyrings/postgresql-keyring.gpg] http://apt.postgresql.org/pub/repos/apt/ bullseye-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   apt-get update
-  apt-get install -y postgresql-client-11
+  apt-get install -y postgresql-14
 fi
