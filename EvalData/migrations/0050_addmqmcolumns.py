625d0f7e27a4348d7ede5e1b7694f059349797ba
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-02-08 18:53:07 +0100

    pass server MQM to the frontend

--- EvalData/migrations/0050_addmqmcolumns.py
+++ EvalData/migrations/0050_addmqmcolumns.py
@@ -24,4 +24,11 @@ class Migration(migrations.Migration):
                 blank=True, null=True, verbose_name='MQM'
             ),
         ),
+        migrations.AddField(
+            model_name='DirectAssessmentDocumentResult',
+            name='mqm',
+            field=models.TextField(
+                blank=True, null=True, verbose_name='MQM'
+            ),
+        ),
     ]
