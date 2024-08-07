695ca53c4287f36b5db66ad445a924ef971fdd0c
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-08-07 13:33:08 +0200

    drop python <3.9 which is EOL

--- EvalData/models/pairwise_assessment_document.py
+++ EvalData/models/pairwise_assessment_document.py
@@ -390,11 +390,7 @@ class PairwiseAssessmentDocumentTask(BaseMetadata):
             # TODO: implement proper support for multiple json files in archive.
             for batch_json_file in batch_json_files:
                 batch_content = batch_zip.read(batch_json_file).decode('utf-8')
-                # Python 3.9 removed 'encoding' from json.loads
-                if sys.version_info >= (3, 9, 0):
-                    batch_json = loads(batch_content)
-                else:
-                    batch_json = loads(batch_content, encoding='utf-8')
+                batch_json = loads(batch_content)
 
         else:
             batch_json = loads(str(batch_file.read(), encoding='utf-8'))
